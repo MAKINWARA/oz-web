bf62bc324ce3d77cd9a3ae5a0a9585ab5cb678d2
Luis Angel Garrido <luangarso@icloud.com>
2024-10-29 15:49:52 -0400

    Config Files

---
+++ astro.config.ts
@@ -0,0 +1,25 @@
+// @ts-check
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+	build: {
+		inlineStylesheets: "always",
+	},
+	compressHTML: true,
+	prefetch: true,
+
+	integrations: [tailwind()],
+	adapter: vercel(),
+	vite: {
+		build: {
+			cssMinify: 'lightningcss'
+		},
+		ssr: {
+			noExternal: ["path-to-regexp"]
+		}
+	}
+})
